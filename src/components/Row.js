2987fedbf59c0a6da6c1086c522a7c8fdcf18128
akshatcr7 <imakshat147@gmail.com>
2020-08-01 00:25:56 +0530

    More Styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -24,8 +24,8 @@ function Row({title, fetchUrl}) {
                 {movies.map(movie => (
                     <img
                     key={movie.id}
-                    className="row_poster" 
-                    src={`${baseUrl}${movie.poster_path}`} alt={movie.name}/>
+                    className={`row_poster ${isLargeRow && "row_poster_large"}`} 
+                    src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
                 ))}
             </div>
         </div>
